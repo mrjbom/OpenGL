8bbd18d353604d2ce359f2a2622caca7e6d36ceb
mrjbom <37502620+mrjbom@users.noreply.github.com>
2019-05-31 11:17:42 +0300

    Create mainWithoutComments.cpp

---
+++ example/mainWithoutComments.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <chrono>
+#include <GL/glut.h>
+
+using namespace std;
+
+constexpr auto FPS_RATE = 60;
+int windowHeight = 600, windowWidth = 600;
+
+void init();
+void idleFunction();
+void displayFunction();
+double getTime();
+
+double getTime()
+{
+	using Duration = std::chrono::duration<double>;
+	return std::chrono::duration_cast<Duration>(
+		std::chrono::high_resolution_clock::now().time_since_epoch()
+		).count();
+}
+
+const double frame_delay = 1.0 / FPS_RATE;
+double last_render = 0;
+
+void init()
+{
+	glutDisplayFunc(displayFunction);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-windowWidth / 2, windowWidth / 2, -windowHeight / 2, windowHeight / 2);
+}
+
+void idleFunction()
+{
+	const double current_time = getTime();
+	if ((current_time - last_render) > frame_delay)
+	{
+		last_render = current_time;
+		glutPostRedisplay();
+	}
+}
+
+void displayFunction()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	
+	/* рисуем */
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutInitWindowPosition((GetSystemMetrics(SM_CXSCREEN) - windowWidth) / 2, (GetSystemMetrics(SM_CYSCREEN) - windowHeight) / 2);
+	glutCreateWindow("Window");
+	init();
+	glutMainLoop();
+	return 0;
+}
